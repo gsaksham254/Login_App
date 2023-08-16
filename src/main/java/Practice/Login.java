10b6507c323f46de01c49719ec0ac002878c818e
saksham <gsaksham254@gmail.com>
2023-08-16 16:34:21 +0530

    Initial Commit

---
+++ src/main/java/Practice/Login.java
@@ -0,0 +1,34 @@
+package Practice;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class Login extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		if(uname.equals("Saksham")&& pass.equals("Rohini")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("username",uname);
+			response.sendRedirect("welcome.jsp");
+			
+		}
+		else {
+			response.sendRedirect("Login.jsp");
+		}
+		
+		
+	}
+
+	
+	
+
+}
